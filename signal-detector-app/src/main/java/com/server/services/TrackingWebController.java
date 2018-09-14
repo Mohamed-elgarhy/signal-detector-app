844bf6520c359685da587d7069bdff9f29b635ed
mohamed ezzeldin <m_elgarhy@yahoo.com>
2018-09-14 19:07:53 +0200

    remove unused files

--- signal-detector-app/src/main/java/com/server/services/TrackingWebController.java
+++ signal-detector-app/src/main/java/com/server/services/TrackingWebController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
